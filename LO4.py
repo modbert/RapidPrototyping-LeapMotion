bafc73a36ce22aa7482476e0c7617b70354a72a0
Nick Aberle <aberle.nick@gmail.com>
2014-02-12 16:04:00 -0700

    added sine wave oscillator to generate sound

--- LO4.py
+++ LO4.py
@@ -19,13 +19,20 @@ import Leap, sys
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 import pygame
 from pygame.locals import *
+from pyo import *
 
 import math
 import numpy
 
+s = Server().boot()
+a = Sine(440, 1, 0.5).out()
+
 
 class SampleListener(Leap.Listener):
+
+
     def on_init(self, controller):
+        s.start()
         print "Initialized"
 
     def on_connect(self, controller):
@@ -57,44 +64,17 @@ class SampleListener(Leap.Listener):
 def initPyGame():
     size = (1366, 720)
 
-    bits = 16
+    #bits = 16
     #the number of channels specified here is NOT 
     #the channels talked about here http://www.pygame.org/docs/ref/mixer.html#pygame.mixer.get_num_channels
 
-    pygame.mixer.pre_init(44100, -bits, 2)
-    pygame.init()
+    #pygame.mixer.pre_init(44100, -bits, 2)
+    #pygame.init()
     _display_surf = pygame.display.set_mode(size, pygame.HWSURFACE | pygame.DOUBLEBUF)
     pass
 
 def makeSounds(leftHz, rightHz):
-    bits = 16
-    duration = 1          # in seconds
-    #freqency for the left speaker
-    frequency_l = leftHz*2
-    #frequency for the right speaker
-    frequency_r = rightHz*2
-
-    #this sounds totally different coming out of a laptop versus coming out of headphones
-
-    sample_rate = 44100
-
-    n_samples = int(round(duration*sample_rate))
-
-    #setup our numpy array to handle 16 bit ints, which is what we set our mixer to expect with "bits" up above
-    buf = numpy.zeros((n_samples, 2), dtype = numpy.int16)
-    max_sample = 2**(bits - 1) - 1
-
-    for s in range(n_samples):
-        t = float(s)/sample_rate    # time in seconds
-
-        #grab the x-coordinate of the sine wave at a given time, while constraining the sample to what our mixer is set to with "bits"
-        buf[s][0] = int(round(max_sample*math.sin(2*math.pi*frequency_l*t)))        # left
-        buf[s][1] = int(round(max_sample*0.5*math.sin(2*math.pi*frequency_r*t)))    # right
-
-    sound = pygame.sndarray.make_sound(buf)
-    #play once, then loop forever
-    sound.play()
-    pass
+    a.setFreq(leftHz*2)
 
 # This function prints the height of each hand (max 2)
 # It will display the outputs in the right order using the x axis data
